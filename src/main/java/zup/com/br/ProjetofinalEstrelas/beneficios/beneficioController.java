656afabfc0693a9af7b981c135b50afde9856893
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 12:39:44 -0300

    Criar metodo salvar na Controller e Refatorar nome Classe Beneficio

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
@@ -1,6 +1,7 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,9 @@ public class beneficioController {
 
 @Autowired
 private beneficioService beneficioService;
+
+
+public Beneficio cadastrarBeneficio (@RequestBody Beneficio beneficio){ return beneficioService.salvarBeneficio(beneficio);}
 
 
 
