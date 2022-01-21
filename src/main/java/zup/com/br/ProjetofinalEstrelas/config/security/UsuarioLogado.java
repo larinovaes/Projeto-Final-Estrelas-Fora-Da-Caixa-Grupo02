edab2e01a0c7ddb5c21c8982f69d796e206b99a4
Larissa Novais <larinovais54@gmail.com>
2022-01-21 16:49:56 -0300

    Ajustes

--- src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/UsuarioLogado.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/UsuarioLogado.java
@@ -12,6 +12,39 @@ public class UsuarioLogado implements UserDetails {
     private String email;
     private String senha;
 
+    public UsuarioLogado(String id, String email, String senha) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public UsuarioLogado() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
