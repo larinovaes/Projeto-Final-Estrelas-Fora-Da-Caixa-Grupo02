16b17cb43927cc93e2b661ec60ad18274540d0b0
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 12:16:06 -0300

    Criar endpoint

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
@@ -1,4 +1,11 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/beneficio")
 public class beneficioController {
+
+
 }
