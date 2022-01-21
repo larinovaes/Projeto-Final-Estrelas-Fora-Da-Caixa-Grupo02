7d26a1109ec38fa74f3b84287761cc39878d945e
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-01-21 11:51:01 -0300

    Torna rota post do usuario publica

--- src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/ConfiguracaoDeSeguranca.java
@@ -1,6 +1,8 @@
 package zup.com.br.ProjetofinalEstrelas.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, "/usuario").permitAll()
+                .anyRequest().authenticated();
+    }
 }
