ef18b851a9be736e303c1ffcbc3f725c06fca3e2
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 13:07:17 -0300

    Criar comportamentos metodos cadastrar e exibir

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
@@ -1,23 +1,27 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/beneficio")
 public class beneficioController {
 
-@Autowired
-private beneficioService beneficioService;
+    @Autowired
+    private beneficioService beneficioService;
 
+    @PostMapping()//C
+    @ResponseStatus(HttpStatus.CREATED)
+    public Beneficio cadastrarBeneficio(@RequestBody Beneficio beneficio) {
+        return beneficioService.salvarBeneficio(beneficio);
+    }
 
-public Beneficio cadastrarBeneficio (@RequestBody Beneficio beneficio){ return beneficioService.salvarBeneficio(beneficio);}
+    @GetMapping//R
+    public Iterable<Beneficio> exibirTodosBeneficios() {
+        return beneficioService.exibirBeneficios();
+    }
 
-public Iterable<Beneficio>exibirTodosBeneficios(){
-return beneficioService.exibirBeneficios();
-}
 
 
 }
