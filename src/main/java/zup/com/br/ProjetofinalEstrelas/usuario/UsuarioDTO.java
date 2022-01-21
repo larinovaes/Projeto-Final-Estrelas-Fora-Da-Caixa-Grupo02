b575371b18aaed3bf9e2fe92e54e6307a4d43f12
Larissa Novais <larinovais54@gmail.com>
2022-01-21 12:49:54 -0300

    Modelar classe dto

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioDTO.java
@@ -1,5 +1,13 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
-public class UsuarioDTO {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Setter
+@Getter
+public class UsuarioDTO {
+    private String email;
+    private String senha;
 }
