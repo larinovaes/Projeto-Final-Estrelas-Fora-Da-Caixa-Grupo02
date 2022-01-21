000e94b4ba5168150f9293a95f3a4a79967cd595
Larissa Novais <larinovais54@gmail.com>
2022-01-21 16:50:51 -0300

    Criar metodo para pegar email

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -2,6 +2,10 @@ package zup.com.br.ProjetofinalEstrelas.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
      void deleteByEmail(String email);
+
+     Optional<Usuario> findByEmail(String email);
 }
