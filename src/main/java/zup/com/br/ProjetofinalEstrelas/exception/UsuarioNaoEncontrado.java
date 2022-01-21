8d2d42db2cfd367ba5e341e768147af88d7dbae0
Larissa Novais <larinovais54@gmail.com>
2022-01-21 12:37:50 -0300

    Criar classe de excessão

--- src/main/java/zup/com/br/ProjetofinalEstrelas/exception/UsuarioNaoEncontrado.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/exception/UsuarioNaoEncontrado.java
@@ -1,2 +1,9 @@
-package zup.com.br.ProjetofinalEstrelas.usuario;public class UsuarioNaoEncontrado {
+package zup.com.br.ProjetofinalEstrelas.exception;
+
+public class UsuarioNaoEncontrado extends RuntimeException {
+    private String menssagem;
+
+    public UsuarioNaoEncontrado(String menssagem) {
+        this.menssagem = menssagem;
+    }
 }
