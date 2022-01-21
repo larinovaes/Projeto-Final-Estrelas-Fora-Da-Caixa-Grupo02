57f3877a7ba93c13a432a6454122d2e07f046787
Larissa Novais <larinovais54@gmail.com>
2022-01-21 14:19:07 -0300

    Retirar comandos para esconder senha

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioService.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioService.java
@@ -1,7 +1,6 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import zup.com.br.ProjetofinalEstrelas.exception.UsuarioNaoEncontrado;
 
@@ -12,13 +11,8 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-    @Autowired
-    private BCryptPasswordEncoder encoder;
 
     public Usuario salvarUsuario(Usuario usuario) {
-        String esconderSenha = encoder.encode(usuario.getSenha());
-
-        usuario.setSenha(esconderSenha);
         return usuarioRepository.save(usuario);
     }
 
@@ -34,10 +28,6 @@ public class UsuarioService {
         if (!usuarioDoBanco.getEmail().equals(usuario.getEmail())) {
             usuarioDoBanco.setEmail(usuario.getEmail());
         }
-
-        String senha = encoder.encode(usuario.getSenha());
-        usuarioDoBanco.setSenha(senha);
-        usuarioRepository.save(usuarioDoBanco);
     }
 
     public void deletarUsuario(String email) {
