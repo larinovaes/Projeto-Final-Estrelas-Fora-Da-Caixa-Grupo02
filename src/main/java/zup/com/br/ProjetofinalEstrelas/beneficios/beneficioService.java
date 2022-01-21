aa6b998f369f0426d4d99bbfa3cb2d44653e3978
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 12:42:32 -0300

    Injetar Repository na Service Subescrever

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioService.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioService.java
@@ -1,4 +1,10 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 public class beneficioService {
+
+@Autowired
+private beneficioRepository beneficioRepository;
 }
