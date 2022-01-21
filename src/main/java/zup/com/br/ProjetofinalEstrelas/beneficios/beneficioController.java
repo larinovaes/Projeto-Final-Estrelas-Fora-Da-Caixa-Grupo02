2d41fbe68c482c56f4aa0825ec71f3cd372267e1
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 12:36:04 -0300

    Injetar Service na Controller e Subescrever

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficioController.java
@@ -1,11 +1,17 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/beneficio")
 public class beneficioController {
+
+@Autowired
+private beneficioService beneficioService;
+
+
 
 
 }
