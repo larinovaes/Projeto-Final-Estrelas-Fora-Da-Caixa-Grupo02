8e173e429fbedc151a150619bc8375adb2c86d7f
Larissa Novais <larinovais54@gmail.com>
2022-01-21 11:57:46 -0300

    Modelar classe controller

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioController.java
@@ -1,4 +1,13 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 }
