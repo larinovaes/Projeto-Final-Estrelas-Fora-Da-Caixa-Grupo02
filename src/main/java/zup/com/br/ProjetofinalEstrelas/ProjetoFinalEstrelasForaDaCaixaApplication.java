f950e8556f88bd225fe09be5ec942f204224fea0
Larissa Novais <larinovais54@gmail.com>
2022-01-21 10:53:03 -0300

    Inicio de projeto

---
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/ProjetoFinalEstrelasForaDaCaixaApplication.java
@@ -0,0 +1,13 @@
+package zup.com.br.ProjetofinalEstrelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalEstrelasForaDaCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalEstrelasForaDaCaixaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/ProjetoFinalEstrelasForaDaCaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.com.br.ProjetofinalEstrelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalEstrelasForaDaCaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
