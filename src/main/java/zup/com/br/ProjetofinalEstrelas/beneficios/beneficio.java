9eaa60bb65cb2e2c295c11de386e3a4cc8fc86ec
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-21 12:03:54 -0300

    Modelar Classe Beneficio

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficio.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/beneficio.java
@@ -1,4 +1,30 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
+import javax.persistence.Column;@
 public class beneficio {
+
+    @Column(nullable = false)
+    private String nome;
+    private String descrição;
+
+    public beneficio(String nome, String descrição) {
+        this.nome = nome;
+        this.descrição = descrição;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
 }
