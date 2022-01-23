40a36b11b919d051d60671401dce74ce8b6e8070
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-23 18:59:11 -0300

    Criar atributos Classe BeneficioControllerTest e add MockMvc

--- src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
@@ -1,5 +1,19 @@
 package zup.com.br.ProjetofinalEstrelas.Beneficio;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
+
+import java.util.List;
+
 public class BeneficioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+    private Beneficio beneficio;
+    private List<Beneficio> beneficios;
 
 }
