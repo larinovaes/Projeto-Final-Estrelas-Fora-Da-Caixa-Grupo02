7b50d46c2054cd0ee66b0815ff0751106f72cb9c
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-23 19:09:11 -0300

    Injetar beneficioController e beneficioService

--- src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
@@ -2,12 +2,19 @@ package zup.com.br.ProjetofinalEstrelas.Beneficio;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
+import zup.com.br.ProjetofinalEstrelas.beneficios.beneficioController;
 
 import java.util.List;
 
+@WebMvcTest(beneficioController.class)
 public class BeneficioControllerTest {
+
+    @MockBean
+    private beneficioService beneficioService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -16,4 +23,6 @@ public class BeneficioControllerTest {
     private Beneficio beneficio;
     private List<Beneficio> beneficios;
 
+    public BeneficioControllerTest() {
+    }
 }
