51315cfefb43d74eee389783b7f9b7775c1a14cf
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-23 19:53:28 -0300

    Criar Classe BeneficioServiceTest

---
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioServiceTest.java
@@ -0,0 +1,17 @@
+package zup.com.br.ProjetofinalEstrelas.Beneficio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
+
+import java.util.List;
+
+@SpringBootTest
+public class BeneficioServiceTest {
+
+@MockBean
+private beneficioRepository beneficioRepository;
+
+
+}
