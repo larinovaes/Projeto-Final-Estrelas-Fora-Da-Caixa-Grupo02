802d24c68fc7817dfe1eba189967b0a9c2690628
Larissa Novais <larinovais54@gmail.com>
2022-01-24 16:35:56 -0300

    retirar metodo de delete

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-     void deleteByEmail(String email);
-
      Optional<Usuario> findByEmail(String email);
 }
