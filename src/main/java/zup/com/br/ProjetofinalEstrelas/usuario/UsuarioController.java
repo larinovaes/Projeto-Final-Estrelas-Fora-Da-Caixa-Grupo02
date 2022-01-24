b7dc57d3b7fab67e7c544e7933c1d598f446714b
Larissa Novais <larinovais54@gmail.com>
2022-01-24 16:44:21 -0300

    Adicionar conversor na controller

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioController.java
@@ -1,7 +1,9 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import zup.com.br.ProjetofinalEstrelas.usuario.dtos.UsuarioDTO;
 
 @RestController
 @RequestMapping("/usuario")
@@ -9,11 +11,15 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.salvarUsuario(usuario);
+    public void cadastrarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
+        usuarioDTO = modelMapper.map(usuario, UsuarioDTO.class);
+        usuarioService.salvarUsuario(usuario);
     }
 
     @GetMapping
@@ -21,9 +27,9 @@ public class UsuarioController {
         return usuarioService.exibirUsuarios();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletarUsuario(String email) {
+    public void deletarUsuario(@PathVariable String email) {
         usuarioService.deletarUsuario(email);
     }
 }
