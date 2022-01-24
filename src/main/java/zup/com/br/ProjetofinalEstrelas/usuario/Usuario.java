86ddffa7512e6eacb939d83bac3dd0c99bc9afcc
Larissa Novais <larinovais54@gmail.com>
2022-01-24 16:35:18 -0300

    Mudar id para email

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/Usuario.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/Usuario.java
@@ -3,7 +3,6 @@ package zup.com.br.ProjetofinalEstrelas.usuario;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -15,11 +14,6 @@ import javax.persistence.*;
 @Getter
 public class Usuario {
     @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    private String id;
-
-    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
