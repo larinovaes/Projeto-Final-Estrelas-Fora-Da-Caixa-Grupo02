2888e6c0966833530fbb6a296cd3a34a682b3793
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-01-24 01:41:12 -0300

    Criar classe de filtro para autenticacao via JWT

---
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/JWT/FiltroDeAutenticacaoJWT.java
@@ -0,0 +1,12 @@
+package zup.com.br.ProjetofinalEstrelas.config.security.JWT;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@AllArgsConstructor
+public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilter {
+    private JWTComponent jwtComponent;
+    private AuthenticationManager authenticationManager;
+}
