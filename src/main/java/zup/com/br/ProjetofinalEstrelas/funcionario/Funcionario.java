b2821c9c484f788d0f388ab97a131703e8b73de7
Larissa Novais <larinovais54@gmail.com>
2022-01-24 14:14:27 -0300

    Adicionar beneficio como lista

--- src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/Funcionario.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/Funcionario.java
@@ -3,12 +3,14 @@ package zup.com.br.ProjetofinalEstrelas.funcionario;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.w3c.dom.stylesheets.LinkStyle;
 import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
 import zup.com.br.ProjetofinalEstrelas.enums.NivelZupper;
 import zup.com.br.ProjetofinalEstrelas.usuario.Usuario;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "funcionario")
@@ -24,8 +26,8 @@ public class Funcionario {
     @OneToOne
     private Usuario usuario;
 
-    @OneToMany
-    private Beneficio beneficio;
+    @OneToMany(cascade = CascadeType.MERGE)
+    private List<Beneficio> beneficios;
 
     @Enumerated(EnumType.STRING)
     private NivelZupper nivelZupper;
