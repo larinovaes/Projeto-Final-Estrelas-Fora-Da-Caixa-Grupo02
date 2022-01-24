c4d03f8aa131f52387360aefc962c0693a854e47
Larissa Novais <larinovais54@gmail.com>
2022-01-24 12:47:23 -0300

    criar interface

---
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioRepository.java
@@ -0,0 +1,6 @@
+package zup.com.br.ProjetofinalEstrelas.funcionario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+}
