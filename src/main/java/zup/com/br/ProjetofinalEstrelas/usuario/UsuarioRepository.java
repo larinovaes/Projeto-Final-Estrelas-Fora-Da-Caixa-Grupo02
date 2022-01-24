b2994ea429821fa0f9cc8a92bd84a21eb18a11e4
Larissa Novais <larinovais54@gmail.com>
2022-01-24 17:31:00 -0300

    Retirar metodo da repository

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-     Optional<Usuario> findByEmail(String email);
 }
