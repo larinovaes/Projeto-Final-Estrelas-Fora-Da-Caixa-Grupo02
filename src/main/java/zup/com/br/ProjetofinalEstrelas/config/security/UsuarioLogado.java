0251398fa3bec2564ceabcd468afc5850ccddf29
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-01-24 01:21:30 -0300

    Remover get/set

--- src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/UsuarioLogado.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/config/security/UsuarioLogado.java
@@ -12,38 +12,6 @@ public class UsuarioLogado implements UserDetails {
     private String email;
     private String senha;
 
-    public UsuarioLogado(String id, String email, String senha) {
-        this.id = id;
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public UsuarioLogado() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
