44f693c7e42caaa817cfa9e37791b0c77ff123c6
Larissa Novais <larinovais54@gmail.com>
2022-01-25 16:28:45 -0300

    Criar metodo para buscar funcionario

--- src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioRepository.java
@@ -2,5 +2,8 @@ package zup.com.br.ProjetofinalEstrelas.funcionario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+    Optional<Funcionario> findById(Integer id);
 }
