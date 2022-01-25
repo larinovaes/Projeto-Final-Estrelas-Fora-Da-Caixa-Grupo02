286e1a658cd5d273f3fb37774bbf829827e685ec
Larissa Novais <larinovais54@gmail.com>
2022-01-25 11:52:00 -0300

    Adicionar validação na classe de Login

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/dtos/LoginDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/dtos/LoginDTO.java
@@ -5,14 +5,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class LoginDTO {
-    @Email
+    @Email(message = "{validacao.email.email}")
     private String email;
-    @NotNull
+
+    @NotBlank(message = "{validacao.senha.not-blank}")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$" , message = "Senha inválida")
     private String senha;
 }
