02eef8b8019afed6e633e428d526f3af42705c8e
Larissa Novais <larinovais54@gmail.com>
2022-01-25 16:48:07 -0300

    Adicionar metodo findByEmail

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -3,5 +3,5 @@ package zup.com.br.ProjetofinalEstrelas.usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-
+    Usuario findByemail(String email);
 }
