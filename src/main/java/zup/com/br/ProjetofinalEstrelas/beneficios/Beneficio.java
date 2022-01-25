aa982f810944f9e2e3c40a1b76b1bac4d2036ed5
Larissa Novais <larinovais54@gmail.com>
2022-01-25 15:18:13 -0300

    Adicionar nivel de zupper e validações

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/Beneficio.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/Beneficio.java
@@ -1,52 +1,34 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import zup.com.br.ProjetofinalEstrelas.enums.NivelZupper;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "beneficio")
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Beneficio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(nullable = false)
     @Size(min = 3,message = "O nome deve ter no mínimo 3 caracteres")
     private String nome;
+
+    @NotBlank
     private String descricao;
+    @NotNull
+    private NivelZupper nivelZupper;
 
-
-    public Beneficio() {
-    }
-
-    public Beneficio(int id, String nome, String descricao) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-
-    }
 }
