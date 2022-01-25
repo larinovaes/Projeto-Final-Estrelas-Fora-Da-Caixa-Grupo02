8825238c938edd893f144e5fea4e96a76b74f421
Larissa Novais <larinovais54@gmail.com>
2022-01-25 10:35:52 -0300

    Ajustes de intentação

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -2,7 +2,6 @@ package zup.com.br.ProjetofinalEstrelas.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
+
 }
