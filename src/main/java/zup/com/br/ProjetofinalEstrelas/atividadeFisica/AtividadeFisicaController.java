8467cb1ee040750de2370d7b449cd370c99ce72f
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-25 16:00:13 -0300

    Criar metodo cadastrar Atividade Fisica na Controller

--- src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/AtividadeFisicaController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/AtividadeFisicaController.java
@@ -3,6 +3,10 @@ package zup.com.br.ProjetofinalEstrelas.atividadeFisica;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class AtividadeFisicaController {
 
@@ -11,6 +15,13 @@ public class AtividadeFisicaController {
     private AtividadeFisicaService atividadeFisicaService;
     @Autowired
     ModelMapper modelMapper;
+
+
+    @PostMapping()//C
+    @ResponseStatus(HttpStatus.CREATED)
+    public AtividadeFisica cadastrarAtividadeFisica(@RequestBody AtividadeFisica atividadeFisica) {
+        return atividadeFisicaService.salvarAtividadeFisica(atividadeFisica);
+    }
 
 
 
