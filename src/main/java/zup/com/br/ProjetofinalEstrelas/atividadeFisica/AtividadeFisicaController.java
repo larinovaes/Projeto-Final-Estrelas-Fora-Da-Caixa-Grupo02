85267b23ca32090b0f2bce9499c171718aa28195
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-25 15:57:17 -0300

    Injetar dependências Controller

--- src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/AtividadeFisicaController.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/AtividadeFisicaController.java
@@ -1,4 +1,17 @@
 package zup.com.br.ProjetofinalEstrelas.atividadeFisica;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AtividadeFisicaController {
+
+
+    @Autowired
+    private AtividadeFisicaService atividadeFisicaService;
+    @Autowired
+    ModelMapper modelMapper;
+
+
+
 }
