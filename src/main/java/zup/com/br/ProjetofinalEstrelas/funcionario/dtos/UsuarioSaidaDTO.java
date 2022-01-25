16bdc0c8cc137569bd7d58fc911e9de857a71c10
Larissa Novais <larinovais54@gmail.com>
2022-01-25 16:45:16 -0300

    Criar classe UsuarioSaidaDTO

--- src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/dtos/UsuarioSaidaDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/dtos/UsuarioSaidaDTO.java
@@ -1,2 +1,15 @@
-package zup.com.br.ProjetofinalEstrelas.funcionario.dtos;public class UsuarioSaidaDTO {
+package zup.com.br.ProjetofinalEstrelas.funcionario.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UsuarioSaidaDTO {
+    @Email(message = "${validacao.email.email}")
+    private String email;
 }
