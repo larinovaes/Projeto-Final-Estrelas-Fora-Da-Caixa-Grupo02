1cc6651edac281cab9929f68bb2fb152d8226ed3
Larissa Novais <larinovais54@gmail.com>
2022-01-25 16:15:54 -0300

    Retirar atributo beneficios da classe funcionario

--- src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/Funcionario.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/Funcionario.java
@@ -3,13 +3,11 @@ package zup.com.br.ProjetofinalEstrelas.funcionario;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
 import zup.com.br.ProjetofinalEstrelas.enums.NivelZupper;
 import zup.com.br.ProjetofinalEstrelas.usuario.Usuario;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Table(name = "funcionario")
@@ -24,9 +22,6 @@ public class Funcionario {
 
     @OneToOne
     private Usuario usuario;
-
-    @OneToMany(cascade = CascadeType.MERGE)
-    private List<Beneficio> beneficios;
 
     @Enumerated(EnumType.STRING)
     private NivelZupper nivelZupper;
