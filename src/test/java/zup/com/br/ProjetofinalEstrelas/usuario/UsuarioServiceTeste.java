d7f4003fc1c1da56d2a332778f57152b0c313281
Larissa Novais <larinovais54@gmail.com>
2022-01-26 14:01:56 -0300

    Criar metodo para setar o objeto

--- src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
@@ -1,5 +1,6 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,5 +14,12 @@ public class UsuarioServiceTeste {
     private UsuarioRepository usuarioRepository;
 
     private Usuario usuario;
+
+    @BeforeEach
+    private void setup() {
+        usuario = new Usuario();
+        usuario.setEmail("larissa@zup.com");
+        usuario.setSenha("Larissa@123");
+    }
 
 }
