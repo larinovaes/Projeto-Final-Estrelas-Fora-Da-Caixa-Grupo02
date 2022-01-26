9876d7f0b7a9428c91f5e2752a30fbfc1af78995
Larissa Novais <larinovais54@gmail.com>
2022-01-26 13:57:38 -0300

    Adicionar injeção de dependencia e atributo

--- src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
@@ -1,4 +1,17 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
 public class UsuarioServiceTeste {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @MockBean
+    private UsuarioRepository usuarioRepository;
+
+    private Usuario usuario;
+
 }
