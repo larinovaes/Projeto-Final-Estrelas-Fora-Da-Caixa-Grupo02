431b1198db0982511c057619dda1f591dbd43041
Larissa Novais <larinovais54@gmail.com>
2022-01-26 14:10:01 -0300

    Verifica se usuario está sendo salvo

--- src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioServiceTeste.java
@@ -1,6 +1,8 @@
 package zup.com.br.ProjetofinalEstrelas.usuario;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +22,15 @@ public class UsuarioServiceTeste {
         usuario = new Usuario();
         usuario.setEmail("larissa@zup.com");
         usuario.setSenha("Larissa@123");
+    }
+
+    @Test
+    public void testarCadastrarUsuario() {
+        Mockito.when(usuarioRepository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+
+        Usuario usuarioObjeto = usuarioService.salvarUsuario(usuario);
+
+        Mockito.verify(usuarioRepository, Mockito.times(1)).save(Mockito.any(Usuario.class));
     }
 
 }
