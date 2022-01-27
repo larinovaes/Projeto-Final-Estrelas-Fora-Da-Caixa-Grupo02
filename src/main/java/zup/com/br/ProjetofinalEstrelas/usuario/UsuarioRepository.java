f003c82b3f484faf050b2e561be4c98f687d4e12
Larissa Novais <larinovais54@gmail.com>
2022-01-27 14:18:03 -0300

    Remover metodo da usuarioRepository

--- src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/usuario/UsuarioRepository.java
@@ -3,5 +3,4 @@ package zup.com.br.ProjetofinalEstrelas.usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
-    Usuario findByemail(String email);
 }
