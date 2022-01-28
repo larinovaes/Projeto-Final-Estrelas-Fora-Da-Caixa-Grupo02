5377f2b8f7762c66a1d18d434793b625980468d2
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-01-28 14:02:31 -0300

    Refatorar dtos de beneficio para usar lombok

--- src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/dtos/BeneficioDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/beneficios/dtos/BeneficioDTO.java
@@ -1,9 +1,9 @@
 package zup.com.br.ProjetofinalEstrelas.beneficios.dtos;
+import lombok.Data;
 
-import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
+@Data
 public class BeneficioDTO {
 
     @NotBlank(message = "{validacao.not-blank}")
@@ -13,30 +13,4 @@ public class BeneficioDTO {
     @NotBlank(message = "{validacao.not-blank}")
     private String descrição;
 
-    public BeneficioDTO() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescrição() {
-        return descrição;
-    }
-
-    public void setDescrição(String descrição) {
-        this.descrição = descrição;
-    }
 }
