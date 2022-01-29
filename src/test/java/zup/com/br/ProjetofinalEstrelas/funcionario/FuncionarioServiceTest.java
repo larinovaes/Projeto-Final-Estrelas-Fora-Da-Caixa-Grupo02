ce6b873f9b504c72a1cc7300302eab80283d4b96
Larissa Novais <larinovais54@gmail.com>
2022-01-28 21:37:54 -0300

    Criar o setUp do Funcionarioteste

--- src/test/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioServiceTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioServiceTest.java
@@ -1,10 +1,13 @@
 package zup.com.br.ProjetofinalEstrelas.funcionario;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import zup.com.br.ProjetofinalEstrelas.enums.NivelZupper;
+import zup.com.br.ProjetofinalEstrelas.usuario.Usuario;
 
 import java.time.LocalDate;
 
@@ -19,9 +22,11 @@ public class FuncionarioServiceTest {
 
     @BeforeEach
     public void setUp() {
+        funcionario = new Funcionario();
         funcionario.setId(1);
         funcionario.setUsuario(funcionario.getUsuario());
         funcionario.setNivelZupper(NivelZupper.ZUPPER3);
         funcionario.setDataDeContratacao(LocalDate.now());
     }
+
 }
