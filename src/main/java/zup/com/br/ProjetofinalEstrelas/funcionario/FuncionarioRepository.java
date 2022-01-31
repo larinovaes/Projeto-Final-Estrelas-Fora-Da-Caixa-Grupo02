ff4225c21212a3d8377d7eb61cc34fc75d541c9b
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-01-31 14:09:51 -0300

    Implementar metodo de filtro na repository

--- src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioRepository.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
     Optional<Funcionario> findById(Integer id);
+    Optional<Funcionario> findByUsuarioEmail(String email);
 }
