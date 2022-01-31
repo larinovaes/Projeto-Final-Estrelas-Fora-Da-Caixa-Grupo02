48d85c0d1295d0e14269bc19daaac6754e879604
Larissa Novais <larinovais54@gmail.com>
2022-01-31 16:00:12 -0300

    Modelar classe de funcionarioControllerTest

--- src/test/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/funcionario/FuncionarioControllerTest.java
@@ -1,13 +1,29 @@
 package zup.com.br.ProjetofinalEstrelas.funcionario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 import zup.com.br.ProjetofinalEstrelas.componente.ConversorModelMapper;
 import zup.com.br.ProjetofinalEstrelas.config.security.JWT.JWTComponent;
 import zup.com.br.ProjetofinalEstrelas.config.security.UsuarioLoginService;
+import zup.com.br.ProjetofinalEstrelas.funcionario.dtos.FuncionarioDTO;
+import zup.com.br.ProjetofinalEstrelas.funcionario.dtos.FuncionarioEntradaDTO;
 import zup.com.br.ProjetofinalEstrelas.usuario.UsuarioController;
 
 @WebMvcTest({UsuarioController.class, ConversorModelMapper.class, UsuarioLoginService.class, JWTComponent.class})
 public class FuncionarioControllerTest {
+    @MockBean
+    private FuncionarioService funcionarioService;
+    @MockBean
+    private UsuarioLoginService usuarioLoginService;
+    @MockBean
+    private JWTComponent jwtComponent;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Funcionario funcionario;
+    private FuncionarioDTO funcionarioDTO;
+    private FuncionarioEntradaDTO funcionarioEntradaDTO;
 }
