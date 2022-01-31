7581833f356e05582597854055818de86de3c0d3
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-31 15:26:50 -0300

    Ajuste BeneficioControllerTest

--- src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -38,6 +39,8 @@ public class BeneficioControllerTest {
     private UsuarioLoginService usuarioLoginService;
     @MockBean
     private JWTComponent jwtComponent;
+    @MockBean
+    ModelMapper modelMapper;
 
     @Autowired
     private MockMvc mockMvc;
