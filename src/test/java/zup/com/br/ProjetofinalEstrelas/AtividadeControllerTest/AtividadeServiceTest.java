c8a1f72f13266d04d45b7bbddce79e73037be50b
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-01 13:57:38 -0300

    Injetar Dependências AtividadeServiceTest

--- src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeServiceTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeServiceTest.java
@@ -1,7 +1,21 @@
 package zup.com.br.ProjetofinalEstrelas.AtividadeControllerTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisicaRepository;
+import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisicaService;
+import zup.com.br.ProjetofinalEstrelas.usuario.UsuarioService;
 
 @SpringBootTest
 public class AtividadeServiceTest {
+
+    @MockBean
+    private AtividadeFisicaRepository atividadeFisicaRepository;
+
+    @MockBean
+    private AtividadeFisicaService atividadeFisicaService;
+
+    @MockBean
+    private UsuarioService usuarioService;
 }
