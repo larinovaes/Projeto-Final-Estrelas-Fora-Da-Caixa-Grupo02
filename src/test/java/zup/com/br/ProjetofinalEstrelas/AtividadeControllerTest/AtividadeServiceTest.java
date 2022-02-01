30c366962303871b092cccd0442ae662c3db3ef0
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-01 13:59:38 -0300

    Criar Atributos AtividadeServiceTest

--- src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeServiceTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeServiceTest.java
@@ -3,9 +3,12 @@ package zup.com.br.ProjetofinalEstrelas.AtividadeControllerTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisica;
 import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisicaRepository;
 import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisicaService;
 import zup.com.br.ProjetofinalEstrelas.usuario.UsuarioService;
+
+import java.util.List;
 
 @SpringBootTest
 public class AtividadeServiceTest {
@@ -18,4 +21,8 @@ public class AtividadeServiceTest {
 
     @MockBean
     private UsuarioService usuarioService;
+
+    private AtividadeFisica atividadeFisica;
+    private List<AtividadeFisica> atividades;
+
 }
