53acb3d5f007fe5f3b18ef0ae2b70e1af2c5ca1a
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-01 13:30:55 -0300

    Criar Validacoes NotBLanck EndPoint AtividadeFisica

--- src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/dtos/AtividadeFisicaDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/dtos/AtividadeFisicaDTO.java
@@ -5,18 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AtividadeFisicaDTO {
 
-
+    @NotBlank(message = "{validacao.not-blank}")
     private String nome;
+    @NotBlank(message = "{validacao.not-blank}")
     private String cidade;
+    @NotBlank(message = "{validacao.not-blank}")
     private String bairro;
+    @NotBlank(message = "{validacao.not-blank}")
     private String horario;
+    @NotBlank(message = "{validacao.not-blank}")
     private String endereco;
+    @NotBlank(message = "{validacao.not-blank}")
     private String responsavel;
+    @NotBlank(message = "{validacao.not-blank}")
     private String contato;
 
 
--- src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/dtos/ExibirDetalheAtividadeFisicaDTO.java
+++ src/main/java/zup/com/br/ProjetofinalEstrelas/atividadeFisica/dtos/ExibirDetalheAtividadeFisicaDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,13 +14,19 @@ import lombok.Setter;
 
 public class ExibirDetalheAtividadeFisicaDTO {
 
-
+    @NotBlank(message = "{validacao.not-blank}")
     private String nome;
+    @NotBlank(message = "{validacao.not-blank}")
     private String cidade;
+    @NotBlank(message = "{validacao.not-blank}")
     private String bairro;
+    @NotBlank(message = "{validacao.not-blank}")
     private String horario;
+    @NotBlank(message = "{validacao.not-blank}")
     private String endereco;
+    @NotBlank(message = "{validacao.not-blank}")
     private String responsavel;
+    @NotBlank(message = "{validacao.not-blank}")
     private String contato;
 
 }
