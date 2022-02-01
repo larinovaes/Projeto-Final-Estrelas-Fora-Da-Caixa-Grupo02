c4507bb1fab1ef9a1bc4caa31c52ce04623a8ebf
Bárbara Ann Lima <babiplima@gmail.com>
2022-02-01 12:13:42 -0300

    Criar Atributos Classe AtividadeControllerTest

--- src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeControllerTest.java
@@ -1,4 +1,52 @@
 package zup.com.br.ProjetofinalEstrelas.AtividadeControllerTest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import zup.com.br.ProjetofinalEstrelas.atividadeFisica.AtividadeFisica;
+import zup.com.br.ProjetofinalEstrelas.atividadeFisica.dtos.AtividadeFisicaDTO;
+import zup.com.br.ProjetofinalEstrelas.beneficios.Beneficio;
+import zup.com.br.ProjetofinalEstrelas.beneficios.BeneficioController;
+import zup.com.br.ProjetofinalEstrelas.beneficios.BeneficioService;
+import zup.com.br.ProjetofinalEstrelas.beneficios.dtos.BeneficioDTO;
+import zup.com.br.ProjetofinalEstrelas.componente.ConversorModelMapper;
+import zup.com.br.ProjetofinalEstrelas.config.security.JWT.JWTComponent;
+import zup.com.br.ProjetofinalEstrelas.config.security.UsuarioLoginService;
+import zup.com.br.ProjetofinalEstrelas.enums.NivelZupper;
+import zup.com.br.ProjetofinalEstrelas.exception.BeneficioNaoEncontradoException;
+import zup.com.br.ProjetofinalEstrelas.usuario.UsuarioService;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class AtividadeControllerTest {
+
+    @MockBean
+    private BeneficioService beneficioService;
+    @MockBean
+    private UsuarioLoginService usuarioLoginService;
+    @MockBean
+    private JWTComponent jwtComponent;
+    @MockBean
+    ModelMapper modelMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+    private AtividadeFisica;
+    private AtividadeFisicaDTO;
+
 }
