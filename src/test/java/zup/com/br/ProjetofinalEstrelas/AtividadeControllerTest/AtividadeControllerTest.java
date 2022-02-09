5bc95f8eb3a9ba5a661816a70b61c12a67f6e376
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-02-09 20:10:12 -0300

    Criar teste para nome not null

--- src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/AtividadeControllerTest/AtividadeControllerTest.java
@@ -266,6 +266,19 @@ public class AtividadeControllerTest {
                 .andExpect(MockMvcResultMatchers.status().is(422));
     }
 
+    @Test
+    @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+    public void testarCadastrarAtividadeFisicaValidarNomeNotNull() throws Exception {
+        atividadeFisicaDTO.setNome(null);
+
+        Mockito.when((atividadeFisicaService.salvarAtividadeFisica(Mockito.any(AtividadeFisica.class)))).thenReturn(atividadeFisica);
+        String json = objectMapper.writeValueAsString(atividadeFisicaDTO);
+
+        ResultActions resposta = mockMvc.perform(MockMvcRequestBuilders.post("/atividadefisica")
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(422));
+    }
+
   @Test
     @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
     public void testarDeletarAtividadeFisicaNaoEncontrada() throws Exception {
