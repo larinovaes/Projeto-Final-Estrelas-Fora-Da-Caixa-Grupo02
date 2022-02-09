a43925d03f3c3052929b73fe922bd930b5bdf8ac
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2022-02-09 18:12:21 -0300

    Criar teste para descricao not null

--- src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
+++ src/test/java/zup/com/br/ProjetofinalEstrelas/Beneficio/BeneficioControllerTest.java
@@ -184,6 +184,18 @@ public class BeneficioControllerTest {
 
     @Test
     @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+    public void testarCadastroDeBeneficioValidarDescricaoNotNull() throws Exception {
+        beneficioDTO.setDescricao(null);
+        Mockito.when((beneficioService.salvarBeneficio(Mockito.any(Beneficio.class)))).thenReturn(beneficio);
+        String json = objectMapper.writeValueAsString(beneficioDTO);
+
+        ResultActions resposta = mockMvc.perform(MockMvcRequestBuilders.post("/beneficio")
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(422));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
     public void testarDeletarBeneficioNaoEncontrado() throws Exception {
         Mockito.doThrow(BeneficioNaoEncontradoException.class).when(beneficioService).deletarBeneficio(Mockito.anyInt());
 
